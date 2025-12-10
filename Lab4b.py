db04c762e440a0ba5ce0b84befa9c67ad63421db
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2025-12-10 12:34:28 +0530

    added the histogram i.r 4th b lab pgm

---
+++ Lab4b.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.title("sales v/s price with profit")
+
+price = np.asarray([2.5, 1.23, 4.02, 3.35, 5.00, 4.44])
+sales_per_day = np.asarray([34, 64, 49, 22, 19, 13])
+profit = np.asarray([20, 35, 40, 20, 27, 5])  # matched length
+
+plt.scatter(
+    x=price,
+    y=sales_per_day,
+    s=profit * 20,
+    c='r',
+    edgecolors='b',
+    marker='*'   # ⭐ star marker
+)
+
+plt.xlabel("chocolates price", fontsize=15)
+plt.ylabel("sales per day", fontsize=15)
+plt.xticks(price)
+plt.yticks(sales_per_day)
+plt.show()
