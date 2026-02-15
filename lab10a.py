5797bdfcc12183582ab032685e704750e49dd147
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2026-02-15 10:14:39 +0530

    updated the 10th lab pgm

--- lab10a.py
+++ lab10a.py
@@ -1,15 +1,17 @@
 import plotly.express as px
-import pandas as pd
 
 df = px.data.stocks()
 print(df.head(5))
-fig = px.line(df,x="date",y=df.columns,hover_data={"date":"|%B %d, %Y"},title='custom tick labels')
+
+fig = px.line(df,x = "date",y = df.columns,hover_data = {"date":"|%b %d,%Y"})
 fig.update_xaxes(
-    dtick="M1",
-    tickformat="%b \n %Y"
+    dtick = 'M1',
+    tickformat = "%b \n %y"
 )
 fig.show()
+
 fig = px.scatter(df,x="date",y=df.columns)
 fig.show()
+
 fig = px.bar(df,x="date",y=df.columns,barmode='group')
 fig.show()
