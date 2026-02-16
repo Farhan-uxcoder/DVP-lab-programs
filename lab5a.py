beedfd5ce7dea8542daf47f0ff6f6a8aad852b03
Farhan-uxcoder <mohammedfarhan1089@gmail.com>
2026-02-16 13:34:32 +0530

    updated lab5

--- lab5a.py
+++ lab5a.py
@@ -3,17 +3,14 @@ import numpy as np
 
 grades = [70,71,72,78,79,80,88, 92, 95, 97, 85, 90, 93, 99, 100, 91, 94]
 
-plt.figure(figsize=(8, 6))
 plt.hist(
     grades,
     bins=[70, 75, 80, 85, 90, 95, 100],
-    edgecolor='black',
-    alpha=0.7,
-    color='skyblue'
-)
+    edgecolor = 'k'
+    )
 
 plt.xlabel("Grades")
 plt.ylabel("Frequency")
 plt.title("Grades in a Class")
-plt.xlim(70, 100)
+
 plt.show()
